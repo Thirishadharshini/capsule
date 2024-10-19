f74aa4f0e1ce81917407492678249fd48dd446eb
3sha dharshini <thirishadharshinim@gmail.com>
2024-10-19 12:53:02 +0530

    commit

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@ int main()
 {
     
      color_difference cd;
+     //give your input path directory
      cd.split_images("C:\\office works\\color_check\\cropped\\Image_20241014110326593.jpg\\");
      cd.split_images("C:\\office works\\color_check\\cropped\\Image_20241014110348798.jpg\\");
      cd.split_images("C:\\office works\\color_check\\cropped\\Image_20241014110552648.jpg\\");
